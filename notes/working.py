035803b84047d9fc0672b8272fc57c1313688759
Madalyn Bartman <105222619+madalynbartman@users.noreply.github.com>
2024-12-05 15:32:02 -0700

    Notes

--- notes/working.py
+++ notes/working.py
@@ -2,16 +2,6 @@ from fastapi import FastAPI, Path, Query, HTTPException, status
 from typing import Optional
 from pydantic import BaseModel
 
-# What is an API? 
-# A web service that provides an interface to applications to manipulate and retrive info
-# Rather than writing 5 inventory management services for every app that uses inventory, write one and they can access it via the api.
-# A request is sent to the API and the API sends all of that info to the front
-# This way you dont need 5 different backends sending th e5 apps inventory info. You have one API that they can all query
-
-# Waht data does an API exchange?
-# JSON: Javascript Object Notation. FastAPI handles jsonifying our info so we can work in python types. FastAPI handles that conversion from JSON to a standard python dictionary for us
-
-# create an app object to initialize our API. Need to do this when starting with FastAPI
 app = FastAPI()
 
 class Item(BaseModel):
@@ -24,7 +14,7 @@ class UpdateItem(BaseModel):
     price: Optional[float] = None
     brand: Optional[str] = None
 
-# not the best way to have an inventory
+# Not the best way to have an inventory
 # inventory = {
 #     1: {
 #         "name": "Milk",
@@ -32,38 +22,16 @@ class UpdateItem(BaseModel):
 #         "brand": "Regular"
 #     }
 # }
-# better way
+# Better way
 inventory = {}
 
-# Decorators modify a function without modifying their source code. They're applied in the order that we've called them.
 
-# What is an endpoint?
-# the ending path after the main domain
-# you go there and something happens
-
-# Before the endpoint, you need to write app dot and then the method that this endpoint is going to accept 
-
-# What is http? 
-# hyper text transfer protocol. whenever you set up an endpoint you can set it up to be a different method 
-
-# The four core http methods: 
-# GET: when you set up a get request or you have an endpoint that has a get method what this means is that this endpoint is going to be returning information
-# POST: means that you're going to be sending information to the post endpoint. POST will be creating something new adding something to the database. maybe posting a new user login or a new user sign up.
-# PUT: this is to actually update something that's already existing in the database.  modify information essentially 
-# DELETE: getting rid of information
-
-# you're going to say @app dot, then the method, then the endpoint
-# /: root or your home endpoint
-# make sure that your endpoint is right above the function that will be triggered when you go to it
-
-# Path parameters: used to identify a specific resource or resource
+# Path parameters
 @app.get("/get-item/{item_id}/{name}")
 def get_item(item_id: int = Path(description="The ID of the item you'd like to view")):
     return inventory[item_id]
 
-# query parameters: used to sort, filter, or represent the current page number in a collection. 
-# They are optional parameters that are attached to the end of a URL after a question mark. 
-# They are separated by the ampersand (&) symbol if there are multiple parameters.
+# Query parameters
 # @app.get("/get-by-name")
 # def get_item(*, name: Optional[str] = None, test: int):
 #     for item_id in inventory:
@@ -71,15 +39,14 @@ def get_item(item_id: int = Path(description="The ID of the item you'd like to v
 #             return inventory[item_id]
 #     return {"Data": "Not found"}
 
-# combine path and query parameters
+# Combine path and query parameters
 @app.get("/get-by-name/{item_id}")
 def get_item(*, item_id: int, name: Optional[str] = None, test: int):
     for item_id in inventory:
         if inventory[item_id]["name"] == name:
             return inventory[item_id]
     # return {"Data": "Not found"}
-# status codes
-# every time you call an http endpoint it will return to you some status code that indicates what happened. the default is 200 that stands for ok
+# Status codes
     raise HTTPException(status_code=404, detail="Item name not found.")
 
 # Request body
@@ -117,6 +84,3 @@ def delete_item(item_id: int = Query(..., description="The ID of the item to del
     
     del inventory[item_id]
     return {"Success": "Item deleted!"}
-
-# run with uvicorn working:app --reload
-# reloading means when you save a change in python and hit referesh in the browser it reflects that change
